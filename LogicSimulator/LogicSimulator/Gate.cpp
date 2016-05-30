04cdad02c34a4512b88b603c1191e729cb669d13
우성 황 <zzong2006@gmail.com>
2016-05-30 14:04:46 +0900

    test commit for sourceTree

--- LogicSimulator/LogicSimulator/Gate.cpp
+++ LogicSimulator/LogicSimulator/Gate.cpp
@@ -6,10 +6,13 @@ Gate::Gate()
 {
 	outputCoord.x = 0;
 	outputCoord.y = 0;
+
+	//test input_size
 	for (int i = 0; i < INPUT_SIZE; i++)
 	{
 		input_state[i] = INPUT_SIGNAL;
 	}
+
 	output_state = OUTPUT_SIGNAL;
 	type.SetString(_T("Gate"));
 }
